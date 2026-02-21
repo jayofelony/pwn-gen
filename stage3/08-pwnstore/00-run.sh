c83ee945862fbf4a1271cc28c9c4a3ca6b0b955b
wpa2 <wpa3@protonmail.com>
2026-02-21 13:03:05 +0000

    Stage 08: simplify - just wget pwnstore CLI, pip install handles the rest

--- stage3/08-pwnstore/00-run.sh
+++ stage3/08-pwnstore/00-run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -e
 
+PWNSTORE_RAW="https://raw.githubusercontent.com/wpa-2/pwnagotchi-store/main"
+
 echo -e "\e[32m### Installing PwnStore CLI ###\e[0m"
-wget -q "https://raw.githubusercontent.com/wpa-2/pwnagotchi-store/main/pwnstore.py" \
-    -O "${ROOTFS_DIR}/usr/bin/pwnstore"
+wget -q "${PWNSTORE_RAW}/pwnstore.py" -O "${ROOTFS_DIR}/usr/bin/pwnstore"
 chmod 755 "${ROOTFS_DIR}/usr/bin/pwnstore"
-echo -e "\e[32m### PwnStore CLI installed ###\e[0m"
+
+echo -e "\e[32m### Installing PwnStore UI plugin ###\e[0m"
+wget -q "${PWNSTORE_RAW}/pwnstore_ui.py" \
+    -O "${ROOTFS_DIR}/usr/local/share/pwnagotchi/custom-plugins/pwnstore_ui.py"
+
+echo -e "\e[32m### PwnStore installed ###\e[0m"
