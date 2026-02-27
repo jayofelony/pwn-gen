50daedac0b469765459404feb418034030651d56
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2026-02-27 17:20:55 +0100

    Update installation paths and user environment for Pwnagotchi setup
    Signed-off-by: Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>

--- stage3/05-install-pwnagotchi/01-run-chroot.sh
+++ stage3/05-install-pwnagotchi/01-run-chroot.sh
@@ -8,7 +8,7 @@ cd lg
 make
 make install
 
-cd /home/pi
+cd /opt
 rm -r lg.zip lg/
 
 if [ ! -d pwnagotchi ]; then
@@ -18,17 +18,17 @@ else
     cd /home/pi/pwnagotchi/
     git pull
 fi
-if [ -d /home/pi/.pwn ]; then
-    rm -r /home/pi/.pwn
+if [ -d /opt/.pwn ]; then
+    rm -r /opt/.pwn
 fi
 if [ "$(uname -m)" = "armv6l" ]; then
     export QEMU_CPU=arm1176
 fi
 
 echo -e "\e[32m### Installing python virtual environment ###\e[0m"
-python3 -m venv /home/pi/.pwn/ --system-site-packages
+python3 -m venv /opt/.pwn/ --system-site-packages
 echo -e "\e[32m### Activating virtual environment ###\e[0m"
-source /home/pi/.pwn/bin/activate
+source /opt/.pwn/bin/activate
 
 echo -e "\e[32m### Installing Pwnagotchi ###\e[0m"
 export PATH="/root/.cargo/bin:$PATH"
@@ -40,5 +40,5 @@ deactivate
 
 cd /home/pi
 
-ln -sf /home/pi/.pwn/bin/pwnagotchi /usr/bin/pwnagotchi
-rm -r /home/pi/pwnagotchi
+ln -sf /opt/.pwn/bin/pwnagotchi /usr/bin/pwnagotchi
+rm -r /opt/pwnagotchi
